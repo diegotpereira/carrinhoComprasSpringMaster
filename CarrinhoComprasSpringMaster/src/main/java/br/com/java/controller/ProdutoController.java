05d274833818e9e0c5141331529560bb16fecc3c
Diego <diegoteixeirapereira@hotmail.com>
2021-04-06 10:43:18 -0300

    alteração no arquivo classe controller produto adionado metodo listar.

--- CarrinhoComprasSpringMaster/src/main/java/br/com/java/controller/ProdutoController.java
+++ CarrinhoComprasSpringMaster/src/main/java/br/com/java/controller/ProdutoController.java
@@ -1,15 +1,26 @@
 package br.com.java.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.java.model.Produto;
+import br.com.java.service.ProdutoService;
 
 @Controller
 @RequestMapping("/produto")
 public class ProdutoController {
 	
+	@Autowired
+	private ProdutoService produtoService;
+	
 	@RequestMapping("/produtoLista/todos")
 	public String getProdutos(Model model) {
+		List<Produto> produtos = produtoService.getProdutoLista();
+		model.addAttribute("produtos", produtos);
 		return "produtoLista";
 	}
 	@RequestMapping("/mostrarProduto/")
